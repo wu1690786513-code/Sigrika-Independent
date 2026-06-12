a17f032c63f2618bba50baa4fe90a4da9fd872c4
wu1690786513-code <wu1690786513@gmail.com>
2026-06-12 23:53:37 +0800

    修复conten.config.ts问题

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,1 +1,51 @@
-  
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+
+const postsCollection = defineCollection({
+		loader: glob({
+		base: "./src/content/posts",
+		pattern: [
+			"**/*.{md,mdx}",
+			"!**/Template/**",
+			"!**/00_私密日记/**",
+			"!**/00_Attachments/**",
+			"!**/.obsidian/**"
+		]
+		}),
+	schema: z.object({
+		title: z.string(),
+		published: z.date(),
+		updated: z.date().optional(),
+		draft: z.boolean().optional().default(false),
+		description: z.string().optional().default(""),
+		image: z.string().optional().default(""),
+		tags: z.array(z.string()).optional().default([]),
+		category: z.string().optional().nullable().default(""),
+		lang: z.string().optional().default(""),
+		pinned: z.boolean().optional().default(false),
+		author: z.string().optional().default(""),
+		sourceLink: z.string().optional().default(""),
+		licenseName: z.string().optional().default(""),
+		licenseUrl: z.string().optional().default(""),
+		comment: z.boolean().optional().default(true),
+		password: z.string().optional().default(""),
+		passwordHint: z.string().optional().default(""),
+
+		/* For internal use */
+		prevTitle: z.string().default(""),
+		prevSlug: z.string().default(""),
+		nextTitle: z.string().default(""),
+		nextSlug: z.string().default(""),
+	}),
+});
+
+const specCollection = defineCollection({
+	loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/spec" }),
+	schema: z.object({}),
+});
+
+export const collections = {
+	posts: postsCollection,
+	spec: specCollection,
+};
