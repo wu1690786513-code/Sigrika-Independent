d7a5bae1d07465005d533b5dae9fef267518651e
Azusa <wu1690786513@gmail.com>
2026-07-28 14:36:30 +0800

    更新友链sigrika.fans

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -225,10 +225,19 @@ export const friendsConfig: FriendLink[] = [
 	"weight": 8,
 	"enabled": true
 	},
-	// 	title: "小曹同学",
-	// 	imgurl: "https://pic.caotx.cn/home/friends/avatars_0_myweb.webp",
-	// 	desc: "春祺夏安 秋绥冬禧",
-	// 	siteurl: "https://blog.caotx.cn/",
+	{
+	"title": "JerryLife",
+	"imgurl": "https://upload.codexing.top/Sigrika/26/07/19/20260719230042167.webp",
+	"desc": "Enjoy life",
+	"siteurl": "https://jerry-nis.top",
+	"tags": ["博客"],
+	"weight": 8,
+	"enabled": true
+	},
+	// 	title: "Sigrika",
+	// 	imgurl: "https://sigrika.fans/avatar.avif",
+	// 	desc: "拉海洛耙耙柑批发中~",
+	// 	siteurl: "https://sigrika.fans/",
 	// 	tags: ["博客"],
 	// 	weight: 8,
 	// 	enabled: true,
